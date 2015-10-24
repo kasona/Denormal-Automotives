14af7b9c95c5369d0b0040b7c0ae1cf966928de3
kasi <kasionaga@gmail.com>
2015-10-23 14:03:15 -1000

    uncommented code

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -3,14 +3,14 @@
 -- CREATE USER denormal_user;
 
 -- 2.Create a new database named denormal_cars owned by denormal_user
--- CREATE DATABASE denormal_cars OWNER denormal_user;
+CREATE DATABASE denormal_cars OWNER denormal_user;
 
 -- 3. Run the provided scripts/denormal_data.sql script on the denormal_cars database
--- \i scripts/denormal_data.sql;
+\i scripts/denormal_data.sql;
 
 -- 4. Inspect the table named car_models by running \dS and look at the data using some SELECT statements
--- \dS
--- SELECT * FROM pg_database;
--- SELECT * FROM pg_attribute;
--- SELECT * FROM pg_language;
--- SELECT * FROM pg_type;
+\dS
+SELECT * FROM pg_database;
+SELECT * FROM pg_attribute;
+SELECT * FROM pg_language;
+SELECT * FROM pg_type;
--- normal_cars.sql
+++ normal_cars.sql
@@ -1,10 +1,13 @@
 -- =============== Modeling a Normalized Schema ===============
 -- 1. Create a new postgres user named normal_user
--- CREATE USER normal_user;
+CREATE USER normal_user;
 
 -- 2. Create a new database named normal_cars owned by normal_user
--- CREATE DATABASE normal_cars OWNER normal_user;
+CREATE DATABASE normal_cars OWNER normal_user;
 
 -- 3. Whiteboard your solution to normalizing the denormal_cars schema.
 
 -- [bonus] Generate a diagram (somehow) in .png (or other) format, that of your normalized cars schema. (save and commit to this repo)
+
+
+-- Attaching picture to exercise
